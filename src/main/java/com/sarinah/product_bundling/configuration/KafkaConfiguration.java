b40cf5063e3c45334195c5299fc5716455fcd1ff
SARINAH\0852595 <aloysius.tanto@sarinah.co.id>
2025-12-19 08:52:50 +0700

    takeout

--- src/main/java/com/sarinah/product_bundling/configuration/KafkaConfiguration.java
+++ src/main/java/com/sarinah/product_bundling/configuration/KafkaConfiguration.java
@@ -9,22 +9,22 @@ import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
-@Configuration
-public class KafkaConfiguration {
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String,String> kafkaListenerContainerFactory(
-            ConsumerFactory<String,String> consumerFactory,
-            KafkaTemplate<String,String> kafkaTemplate
-    ){
-        var factory = new ConcurrentKafkaListenerContainerFactory<String,String>();
-        factory.setConsumerFactory(consumerFactory);
-
-        var recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
-        var errorHandler = new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 5));
-
-        factory.setCommonErrorHandler(errorHandler);
-        return factory;
-    }
-}
+//@Configuration
+//public class KafkaConfiguration {
+//
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String,String> kafkaListenerContainerFactory(
+//            ConsumerFactory<String,String> consumerFactory,
+//            KafkaTemplate<String,String> kafkaTemplate
+//    ){
+//        var factory = new ConcurrentKafkaListenerContainerFactory<String,String>();
+//        factory.setConsumerFactory(consumerFactory);
+//
+//        var recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
+//        var errorHandler = new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 5));
+//
+//        factory.setCommonErrorHandler(errorHandler);
+//        return factory;
+//    }
+//}
 
--- src/main/java/com/sarinah/product_bundling/service/PublishEventService.java
+++ src/main/java/com/sarinah/product_bundling/service/PublishEventService.java
@@ -1,17 +1,48 @@
 package com.sarinah.product_bundling.service;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
-@Component
-public class PublishEventService {
-    private  KafkaTemplate<String,Object> kafkaTemplate;
+import java.util.HashMap;
+import java.util.Map;
 
-    public PublishEventService(KafkaTemplate<String, Object> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
-    public void publishToKafka() {
-        kafkaTemplate.send("omo-notification","sucessfully subtract balance with amount");
-    }
-}
+//@Component
+//public class PublishEventService {
+//    private  KafkaTemplate<String,Object> kafkaTemplate;
+//
+//    private ObjectMapper objectMapper;
+//
+//
+//    public PublishEventService(KafkaTemplate<String, Object> kafkaTemplate, ObjectMapper objectMapper) {
+//        this.kafkaTemplate = kafkaTemplate;
+//        this.objectMapper = objectMapper;
+//    }
+//
+//    @SneakyThrows
+//    public void publishToKafka() {
+//        Map<String, Object> body = new HashMap<>();
+//        body.put("type", "NOTIF_OMO");
+//        body.put("title", "Stock Alert");
+//        body.put("message", "Stock tinggal 5");
+//        body.put("sku", "ABC123");
+//        body.put("qty", 5); // bisa Number / Boolean / nested Map, dll
+//
+//        String json = objectMapper.writeValueAsString(body);
+//        Message<String> message = MessageBuilder
+//                .withPayload(json) // di sini jadi String lagi
+//                .setHeader(KafkaHeaders.TOPIC, "omo-notification")
+//                .setHeader("eventCode", "inventory-inj")
+//                .setHeader("x-channel", "web")
+//                .build();
+//
+//
+//        kafkaTemplate.send(message);
+//    }
+//}
